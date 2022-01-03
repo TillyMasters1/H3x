f39ba42f819706add7575e09d9e1f24ad1d26e7b
Dakota <95160810+urdakota@users.noreply.github.com>
2022-01-03 14:34:54 -0500

    Update Install.lua

--- Install.lua
+++ Install.lua
@@ -1,5 +1,6 @@
 if not isfolder("H3x") then makefolder("H3x") end
 if not isfolder("H3x/Modules") then makefolder("H3x/Modules") end
  if not isfile("H3x/Modules/UI.lua") then writefile("H3x/Modules/UI.lua", game:HttpGet("https://raw.githubusercontent.com/TillyMasters1/H3x/main/Dependancies/UI.lua")) end
+ if not isfile("H3x/Modules/ENV.lua") then writefile("H3x/Modules/ENV.lua", game:HttpGet("https://raw.githubusercontent.com/TillyMasters1/H3x/main/Dependancies/ENV.lua")) end
 if not isfolder("H3x/Config") then makefolder("H3x/Config") end
  if not isfile("H3x/Config/UISettings.txt") then writefile("H3x/Config/UISettings.txt", "P") end

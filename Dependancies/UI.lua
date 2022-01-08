42192da88505e885b3e95e99fd22b8358c495463
Dakota <95160810+urdakota@users.noreply.github.com>
2022-01-08 00:49:46 -0500

    bug

--- Dependancies/UI.lua
+++ Dependancies/UI.lua
@@ -519,11 +519,9 @@ function library:Tab(name)
 		end
 	end)
 	
-	function Window:Button(name,description,image,clicktext,imagerectoffset,imagerectsize,callback)
+	function Window:Button(name,description,image,clicktext,callback)
 		image = image or "rbxassetid://3926305904";
 		callback = callback or function() end;
-		imagerectoffset = imagerectoffset or Vector2.new(0, 0);
-		imagerectsize = imagerectsize or Vector2.new(0, 0);
 
 		local button = {};
 
@@ -625,8 +623,8 @@ function library:Tab(name)
 			Size = UDim2.new(0, 32, 0, 32);
 			ZIndex = 2;
 			Image = image;
-			ImageRectOffset = imagerectoffset
-			ImageRectSize = imagerectsize
+			ImageRectOffset = Vector2.new(124, 484);
+			ImageRectSize = Vector2.new(36, 36)
 		})
 
 		local held = false;

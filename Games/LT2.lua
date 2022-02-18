13df6292b1b661f6bdafc9fff9a39bbf43d9e01d
Carillon <66976036+TillyMasters1@users.noreply.github.com>
2022-02-18 15:19:17 +1030

    Update LT2.lua

--- Games/LT2.lua
+++ Games/LT2.lua
@@ -115,7 +115,7 @@ local Player = library:Tab("Player");
 
 local Slot = library:Tab("Slot");
     Slot:Text("Pink Car Spawner")
-    Slot:Button("Spawn Pink Car","Respawns car until a pink car spawns",function()
+    Slot:Button("Spawn Pink Car","Respawns car until a pink car spawns","Spawn",function()
         if _G.Executed == false or _G.Executed == nil then
             _G.Executed = true
             Spawned = false

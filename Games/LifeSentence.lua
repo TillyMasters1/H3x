11f484e4268bd729a248816166adc06c0cc3964f
Carillon <66976036+TillyMasters1@users.noreply.github.com>
2022-03-02 13:20:24 +1030

    Update LifeSentence.lua

--- Games/LifeSentence.lua
+++ Games/LifeSentence.lua
@@ -7,6 +7,7 @@ game:GetService("Players").LocalPlayer.Backpack.Local.Dead.Disabled = true
 -- Variables
 local plr = game.Players.LocalPlayer
 local Mouse = game:GetService("Players").LocalPlayer:GetMouse()
+local GroupService = game:GetService("GroupService") 
 local torso = plr.Character.Torso
 local ctrl = {f = 0, b = 0, l = 0, r = 0}
 local lastctrl = {f = 0, b = 0, l = 0, r = 0}

47a0c1901de1d81694dc9e32fd919223aae2b2ff
Carillon <66976036+TillyMasters1@users.noreply.github.com>
2022-03-25 22:40:48 +1030

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,5 +1,3 @@
 _G.Games = {
-    ["13822889"] = {Name = "LumberTycoon2", Script = "LT2.lua", ID = 13822889, Status = 4},
-    ["3351674303"] = {Name = "Driving Empire", Script = "DrivingEmpire.lua", ID = 3351674303, Status = 4},
     ["7860844204"] = {Name = "Life Sentence", Script = "LifeSentence.lua", ID = 7860844204, Status = 4},
 }

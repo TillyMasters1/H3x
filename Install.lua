5fc76393fb4eff744113c9870fe274d01b41df2c
Carillon <66976036+TillyMasters1@users.noreply.github.com>
2022-04-18 18:56:22 +0930

    Update Install.lua

--- Install.lua
+++ Install.lua
@@ -1,13 +1,10 @@
-local rq = syn and syn.request or http and http.request or http_request or fluxus and fluxus.request 
+local rq = syn and syn.request or http and http.request or http_request or fluxus and fluxus.request or getgenv().request or request
 
 if not isfolder("H3x") then makefolder("H3x") end
 if not isfolder("H3x/Modules") then makefolder("H3x/Modules") end
 if not isfile("H3x/Modules/ENV.lua") then writefile("H3x/Modules/ENV.lua", game:HttpGet("https://h3x.wtf/Dependancies/ENV.lua")) end
 if not isfile("H3x/Modules/Maid.lua") then writefile("H3x/Modules/Maid.lua", game:HttpGet("https://raw.githubusercontent.com/Quenty/NevermoreEngine/a8a2d2c1ffcf6288ec8d66f65cea593061ba2cf0/Modules/Shared/Events/Maid.lua")) end
-if not isfolder("H3x/Config") then makefolder("H3x/Config") end
-if not isfile("H3x/Config/UISettings.txt") then writefile("H3x/Config/UISettings.txt", "P") end
-
+--[[ if not isfolder("H3x/Config") then makefolder("H3x/Config") end
+if not isfile("H3x/Config/UISettings.txt") then writefile("H3x/Config/UISettings.txt", "P") end ]]--
 local ui = rq({Url = "https://h3x.wtf/Dependancies/UI.lua", Method = "GET"}).Body
-if not isfile("H3x/Modules/UI.lua") or not (readfile("H3x/Modules/UI.lua") == ui) then 
-  writefile("H3x/Modules/UI.lua", ui) 
-end
+if not isfile("H3x/Modules/UI.lua") or not (readfile("H3x/Modules/UI.lua") == ui) then writefile("H3x/Modules/UI.lua", ui) end

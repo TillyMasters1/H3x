a9d8bc209aaa1a3b5497b01799951cab2d3273f4
Carillon <66976036+TillyMasters1@users.noreply.github.com>
2022-04-22 15:23:37 +0930

    Update JailBreak.lua

--- Games/JailBreak.lua
+++ Games/JailBreak.lua
@@ -179,7 +179,7 @@ end)
 
 -- Jewelry Assistant
 local Jewelry = RobAssistant:Section("Jewelry Assistants","","rbxthumb://type=Asset&id=" .. 9427244377 .. "&w=420&h=420","","")
-Jewelry:Switch("Disarm Lasers","Disarm all lasers and cameras from jewelry","rbxassetid://3926307971",false,"164, 84","36, 36",function(e)
+Jewelry:Switch("Disarm Lasers","Disarm all lasers and cameras from jewelry","rbxthumb://type=Asset&id=" .. 9432917379 .. "&w=420&h=420",false,"","",function(e)
     if e == true then
         DisarmJewLasers()
         JewDisarmLasers = true

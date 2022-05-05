f8f06a4da18c267c85d2319c673cbbc5c7d62065
Carillon <66976036+TillyMasters1@users.noreply.github.com>
2022-05-05 12:40:37 +0930

    Update JailBreak.lua

--- Games/JailBreak.lua
+++ Games/JailBreak.lua
@@ -306,7 +306,7 @@ end
 game:GetService("UserInputService").JumpRequest:connect(function()
    if InfJump then
        game.Players.LocalPlayer.Character:FindFirstChildOfClass("Humanoid"):ChangeState("Jumping")
-       repeat wait() until game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.Space) == false
+       repeat wait(1) until game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.Space) == false
    end
 end)
 

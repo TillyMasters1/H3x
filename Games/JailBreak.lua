67792d8ac902385f4e6d5db738d0ae365ec3bb1b
Carillon <66976036+TillyMasters1@users.noreply.github.com>
2022-05-07 21:41:19 +0930

    Update JailBreak.lua

--- Games/JailBreak.lua
+++ Games/JailBreak.lua
@@ -508,7 +508,7 @@ function DisarmBankLasers()
     end
     
     if game:GetService("Workspace").Banks:FindFirstChildWhichIsA("Model").Layout:FindFirstChildWhichIsA("Model"):FindFirstChild("Underwater") then
-        for _,v in ipairs(game:GetService("Workspace").Banks:FindFirstChildWhichIsA("Model").Layout:FindFirstChildWhichIsA("Model").Underwater.EscapeRoutes.BankDoor.Door.Model:GetChildren()) do
+        for _,v in ipairs(game:GetService("Workspace").Banks:FindFirstChildWhichIsA("Model").Layout:FindFirstChildWhichIsA("Model").EscapeRoutes.BankDoor.Door.Model:GetChildren()) do
             if v:FindFirstChild("TouchInterest") then
                 v.Color = Color3.fromRGB(0, 255, 0)
                 v.TouchInterest:Destroy()

1d9a22eb223c73aadd4ed4515306175534cfedc0
Carillon <66976036+TillyMasters1@users.noreply.github.com>
2022-05-17 19:05:35 +0930

    Update UI.lua

--- Dependancies/UI.lua
+++ Dependancies/UI.lua
@@ -2512,7 +2512,7 @@ function library:Tab(name,image,IRO,IRS)
 			Size = UDim2.new(1, 0, 0, 1)
 		})
 
-		colorpicker.Gradient = library:create("ImageLabel",{
+		colorpicker.Gradient = library:create("ImageButton",{
 			Name = "RGB";
 			Parent = colorpicker.Main;
 			AnchorPoint = Vector2.new(0.5, 0.5);

568e71cb2ea4b7ce9e4c9ea66357fdae4c3e8f2f
Carillon <66976036+TillyMasters1@users.noreply.github.com>
2022-05-23 13:00:43 +0930

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,6 +1,6 @@
 _G.Games = {
     ["7860844204"] = {Name = "Life Sentence", Script = "LifeSentence.lua", ID = 7860844204, Status = 4},
-    ["13822889"] = {Name = "Lumber Tycoon 2", Script = "LumberTycoon2.lua", ID = 0, Status = 4},
+    ["13822889"] = {Name = "Lumber Tycoon 2", Script = "LumberTycoon2.lua", ID = 0, Status = 3},
     ["606849621"] = {Name = "Jailbreak", Script = "JailBreak.lua", ID = 606849621, Status = 4},
     ["8916037983"] = {Name = "Starving Artists", Script = "StarvingArtists.lua", ID = 8916037983, Status = 4},
 }

a95193414458962fe9e9a78a2424c714f924f792
Carillon <66976036+TillyMasters1@users.noreply.github.com>
2022-06-06 15:11:16 +0930

    Update JailBreak.lua

--- Games/JailBreak.lua
+++ Games/JailBreak.lua
@@ -1031,7 +1031,7 @@ end)
 -- Rails NoCollide
 function RailsCollide(state)
 	for _,v in ipairs(game.Workspace:GetDescendants()) do
-		if v:IsA("MeshPart") and v.MeshId == "rbxassetid://9808983755" or v.MeshId == "rbxassetid://9809036425" then
+		if v:IsA("MeshPart") and v.Material == Enum.Material.Metal and v.Color == Color3.FromRGB(91,93,105) then
 			v.CanCollide = not state
 		end
 	end	

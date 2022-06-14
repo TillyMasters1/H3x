541076d376b01f0217d276db5418c96986ce4434
Carillon <66976036+TillyMasters1@users.noreply.github.com>
2022-06-14 21:37:57 +0930

    Update Install.lua

--- Install.lua
+++ Install.lua
@@ -3,7 +3,7 @@ local rq = syn and syn.request or http and http.request or http_request or fluxu
 if not isfolder("H3x") then makefolder("H3x") end
 if not isfolder("H3x/Modules") then makefolder("H3x/Modules") end
 if not isfile("H3x/Modules/ENV.lua") then writefile("H3x/Modules/ENV.lua", rq({Url = "https://h3x.wtf/Dependancies/ENV.lua", Method = "GET"}).Body) end
--- if not isfile("H3x/Modules/Maid.lua") then writefile("H3x/Modules/Maid.lua", rq({Url = "https://raw.githubusercontent.com/Quenty/NevermoreEngine/a8a2d2c1ffcf6288ec8d66f65cea593061ba2cf0/Modules/Shared/Events/Maid.lua", Method = "GET"}).Body) end
+if not isfile("H3x/Modules/Maid.lua") then writefile("H3x/Modules/Maid.lua", rq({Url = "https://raw.githubusercontent.com/Quenty/NevermoreEngine/a8a2d2c1ffcf6288ec8d66f65cea593061ba2cf0/Modules/Shared/Events/Maid.lua", Method = "GET"}).Body) end
 --[[ if not isfolder("H3x/Config") then makefolder("H3x/Config") end
 if not isfile("H3x/Config/UISettings.txt") then writefile("H3x/Config/UISettings.txt", "P") end ]]--
 local ui = rq({Url = "https://h3x.wtf/Dependancies/UI.lua", Method = "GET"}).Body
